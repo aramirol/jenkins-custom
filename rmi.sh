2d8d3eec5071c2c44c151cc532531027195b4dbf
aramirol <angel.ramirolatorre@gmail.com>
2022-02-15 16:22:54 +0100

    filter images

--- rmi.sh
+++ rmi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for image in $(docker images aramirol/jenkins-custom | grep -v IMAGE | awk '{print $3}')
+for image in $(docker images aramirol/jenkins-custom | grep -v latest | grep -v IMAGE | awk '{print $3}')
 do
         docker rmi $image -f
 done
